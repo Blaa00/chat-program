94cfd8c7554176954a587ea7562814ba0bcd8fc9
Bla0 <88278955+Blaa00@users.noreply.github.com>
2023-10-10 09:32:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import bec
 
+radio.set_group(10)
+
 public_key, private_key = bec.generateKeyPair(random.randint(50, 80))
 
 connected = False
-
-while !connected:
-    radio.send_string(public_key)
-    basic.pause(1000)
 
 def on_received_string(receivedString):
     if !connected:
@@ -17,7 +15,9 @@ def on_received_string(receivedString):
     serial.write_line(receivedString)
 radio.on_received_string(on_received_string)
 
-radio.set_group(10)
+while !connected:
+    radio.send_string(public_key)
+    basic.pause(1000)
 
 def on_forever():
     radio.send_string(serial.read_line())
